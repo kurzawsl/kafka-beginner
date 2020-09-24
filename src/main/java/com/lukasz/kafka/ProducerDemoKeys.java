a7dd9f61ac702ac0265448bd77f90001f4070b2a
Lukasz Kurzawski <lkurzawski@gmail.com>
2020-09-24 15:33:55 +0200

    kafka producer with keys

---
+++ src/main/java/com/lukasz/kafka/ProducerDemoKeys.java
@@ -0,0 +1,46 @@
+package com.lukasz.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("first_topic", "id_0", "hello world" + i);
+
+            logger.info("Key: " + "id_0");
+
+            producer.send(producerRecord, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp() + "\n"
+                        );
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get();
+        }
+        producer.flush();
+        producer.close();
+
+    }
+}
